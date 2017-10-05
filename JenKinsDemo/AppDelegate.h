e47f427b822a197e4ecaf8b46154d08bc20cac53
jenkins <jenkins@SourceSofts-Mac-mini.local>
2017-10-05 12:03:04 +0530

    Initial Commit

---
+++ JenKinsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenKinsDemo
+//
+//  Created by jenkins on 05/10/17.
+//  Copyright © 2017 jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JenKinsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenKinsDemo
+//
+//  Created by jenkins on 05/10/17.
+//  Copyright © 2017 jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
